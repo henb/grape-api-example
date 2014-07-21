f44962fae493e0b3d765c5ed454bf6f1fc7c87c1
henb <henb2015@gmail.com>
2014-07-21 19:11:11 +0300

    Update files with rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,13 +46,13 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def post_params
-      params.require(:post).permit(:title, :description)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 end
