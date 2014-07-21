45b6ad0fa66b83c7386adcdcbb4557a67eb35835
henb <henb2015@gmail.com>
2014-07-21 19:14:25 +0300

    Add validates for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+  validates :title, uniqueness: true, presence: true, length: { in: 1..100 }
+  validates :description, presence: true, length: { maximum: 1_000 }
 end
