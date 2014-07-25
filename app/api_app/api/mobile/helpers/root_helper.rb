ec9e1b0140bcde107132129b2879b93dd9cd8d3c
henb <henb2015@gmail.com>
2014-07-25 19:16:08 +0300

    Create root_helper

---
+++ app/api_app/api/mobile/helpers/root_helper.rb
@@ -0,0 +1,18 @@
+module API::Mobile::Helpers
+  module RootHelper
+    def response(code, hash={})
+      if hash[:error_message]
+        hash[:status] = 'error'
+        error!(hash, code)
+      else
+        hash[:status] = 'success'
+        good_response(hash, code)
+      end
+    end
+
+    def good_response(hash, code)
+      status code
+      hash
+    end
+  end
+end
