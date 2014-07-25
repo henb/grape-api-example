96e7b7ceab75359527c60cd2dcab6d4072547a8b
henb <henb2015@gmail.com>
2014-07-25 19:31:42 +0300

    Add Post#create & destroy for mobile api

--- app/api_app/api/mobile/posts.rb
+++ app/api_app/api/mobile/posts.rb
@@ -1,17 +1,41 @@
 module API::Mobile
   class Posts < Grape::API
+
+    helpers do
+      attr_accessor :post
+
+      def set_post
+        @post = Post.find(params[:id])
+      end
+    end
+
     namespace :posts do
       desc 'Return all posts'
       get do
         response 200, posts: Post.all
       end
 
-      params { requires :id, type: Integer, desc: "Uniq ID for post" }
+      params { requires :id, type: Integer, desc: 'Uniq ID for post' }
       route_param :id do
+
         desc 'Return post'
         get do
-          response 200, posts: Post.find(params[:id])
+          set_post
+          response 200, post: post
         end
+
+        post :create do
+          post = Post.new title: params[:title], description: params[:description]
+
+          response error_message: post.errors.to_a.join(", ").downcase unless post.save
+          response 201, message: 'Post created!'
+        end
+
+        post :destroy do
+          set_post.destroy
+          response 201, message: 'Post deleted!'
+        end
+
       end
     end
   end
