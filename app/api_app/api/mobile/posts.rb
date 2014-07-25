e92618ca03092ac057b97174312b5ebd59d68bd6
henb <henb2015@gmail.com>
2014-07-25 19:17:13 +0300

    Add post_id for mobile api

--- app/api_app/api/mobile/posts.rb
+++ app/api_app/api/mobile/posts.rb
@@ -5,6 +5,14 @@ module API::Mobile
       get do
         response 200, posts: Post.all
       end
+
+      params { requires :id, type: Integer, desc: "Uniq ID for post" }
+      route_param :id do
+        desc 'Return post'
+        get do
+          response 200, posts: Post.find(params[:id])
+        end
+      end
     end
   end
 end
