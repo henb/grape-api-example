51a82a87b2c3afb88b0df2a531067f8382854802
henb <henb2015@gmail.com>
2014-07-25 19:15:43 +0300

    Update mobile api

--- app/api_app/api/mobile/root.rb
+++ app/api_app/api/mobile/root.rb
@@ -4,8 +4,19 @@ module API::Mobile
     version 'v1', using: :path
     format :json
 
-    post :ping do
+    helpers API::Mobile::Helpers::RootHelper
+
+    rescue_from :all do |e|
+      message = e.message.gsub(/\s*\[.*\Z/, '')
+      Rack::Response.new([{ error_message: e.message, status: "error" }.to_json],
+                          e.respond_to?(:status) ? e.status : 500,
+                          { 'Content-Type' => 'application/json' }).finish
+    end
+
+    get :ping do
       { status: 'ok' }
     end
+
+    mount Posts
   end
 end
